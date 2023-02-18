bf73377870a1db3cabc5f8399379089e6ce8ba8e
Reza Mahmoudi <108347344+R-Mahmoudi@users.noreply.github.com>
2023-02-18 19:25:20 +0330

    add predict view

--- src/backend/server/apps/endpoints/urls.py
+++ src/backend/server/apps/endpoints/urls.py
@@ -1,19 +1,27 @@
-# backend/server/apps/endpoints/urls.py file
-#from django.conf.urls import url, include
-from django.urls import include,path
+from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 
 from apps.endpoints.views import EndpointViewSet
 from apps.endpoints.views import MLAlgorithmViewSet
 from apps.endpoints.views import MLAlgorithmStatusViewSet
 from apps.endpoints.views import MLRequestViewSet
+from apps.endpoints.views import PredictView
+from apps.endpoints.views import ABTestViewSet
+from apps.endpoints.views import StopABTestView
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r"endpoints", EndpointViewSet, basename="endpoints")
 router.register(r"mlalgorithms", MLAlgorithmViewSet, basename="mlalgorithms")
 router.register(r"mlalgorithmstatuses", MLAlgorithmStatusViewSet, basename="mlalgorithmstatuses")
 router.register(r"mlrequests", MLRequestViewSet, basename="mlrequests")
+router.register(r"abtests", ABTestViewSet, basename="abtests")
 
 urlpatterns = [
-    path("^api/v1/endpoints", include(router.urls))
+    url(r"^api/v1/", include(router.urls)),
+    url(
+        r"^api/v1/(?P<endpoint_name>.+)/predict$", PredictView.as_view(), name="predict"
+    ),
+    url(
+        r"^api/v1/stop_ab_test/(?P<ab_test_id>.+)", StopABTestView.as_view(), name="stop_ab"
+    ),
 ]
